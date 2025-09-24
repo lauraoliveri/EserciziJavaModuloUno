3bcc3e800db7a07882cc6c68f9bbabfa36aba601
lauraoliveri <lauraoliveri2003@gmail.com>
2025-09-24 16:47:26 +0200

    Esercizio completato

--- src/ElementoDiMaggioranza.java
+++ src/ElementoDiMaggioranza.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class ElementoDiMaggioranza {
+    public static void main(String[] args) {
+
+        int[] numeri = {4, 4, 2, 4, 7, 4, 4, 9, 4, 1, 4, 3, 4};
+
+        // creo un array per salvare il n di volte che un numero viene ripetuto
+        int[] volte = new int[50];
+
+        for (int i = 0; i < numeri.length; i++) {
+           volte[numeri[i]]++; // alla posizione del numero dell array si aggiunge 1 ( per capire quante volte viene ripetuto)
+        }
+
+        for (int j = 0; j < volte.length; j++) {
+            if ( volte[j] > numeri.length / 2){
+                System.out.println("L'elemento di maggioranza è " + j );
+                System.out.println(j + " è ripetuto " + volte[j] + " volte");
+            }
         }
     }
 }
+
+/*
+Scrivere un programma Java per ottenere l'elemento di maggioranza
+da un lato array di interi contenenti duplicati.
+Elemento di maggioranza: un elemento di maggioranza è un elemento
+che appare più di n / 2 volte dove n è la dimensione dell'array
+ */
